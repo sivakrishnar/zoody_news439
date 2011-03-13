6881b006f38156d9654f1643fa8666e7181d1107
siva k reddybathula <siva.reddybathula@gmail.com>
2011-03-13 01:39:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZoodyNews::Application.config.secret_token = '5dfc28bf03e204e9f24a981aeb97e8e7def3c161b75294bd8cdb64decbd30548f5cf410910ce80880b5e148f674eaa6fb2583d6c20585e9a6cfdf70a810b346d'
